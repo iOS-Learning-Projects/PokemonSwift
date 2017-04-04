72d67622f7694bbc3ea854d75f9c06140f5cdcbd
Bruno Chagas <brunochagas@Mac305.local>
2017-04-04 17:13:46 -0300

    PokeMundo 5

--- PokemonSwift/PokeMundo.swift
+++ PokemonSwift/PokeMundo.swift
@@ -9,13 +9,13 @@
 import Cocoa
 
 class PokeMundo: NSObject {
-    var jogadores: NSArray?
-    var ginasios: NSArray?
-    var pokemons: NSArray?
+    var jogadores: Array<Jogador>?
+    var ginasios: Array<Gym>?
+    var pokemons: Array<Pokemon>?
     
     func iniciarMundo(){
         // Pokemons existentes
-        let bulbasaur = Pokemon("Charmander", ofType: "GRAMA")
+        let bulbasaur = Pokemon("Bulbasaur", ofType: "GRAMA")
         let charmander = Pokemon("Charmander", ofType: "FOGO")
         let squirtle = Pokemon("Squirtle", ofType: "AGUA")
         let poliwag = Pokemon("Poliwag", ofType: "AGUA")
@@ -28,8 +28,7 @@ class PokeMundo: NSObject {
         let paras = Pokemon("Paras", ofType: "GRAMA")
         let exeggcute = Pokemon("Exeggcute", ofType: "GRAMA")
         
-        
-        let iniciais = NSArray(array: [bulbasaur, charmander, squirtle, poliwag, horsea, staryu, growlithe, ponyta, magmar, chikorita, paras, exeggcute])
+        self.pokemons?.append(contentsOf: [bulbasaur, charmander, squirtle, poliwag, horsea, staryu, growlithe, ponyta, magmar, chikorita, paras, exeggcute])
         
         // Lideres
         let pokemonsBrock = NSArray(array: [bulbasaur, paras, exeggcute])
@@ -49,11 +48,8 @@ class PokeMundo: NSObject {
         let cerulean = Gym(gymOfName: "Cerulean", ledBy: misty)
         let vermilion = Gym(gymOfName: "Vermilion", ledBy: ltSurge)
         let celadon = Gym(gymOfName: "Celadon", ledBy: erika)
-        
-        let ginasios = NSArray(array: [pewter, cerulean, vermilion, celadon])
-            
-        self.pokemons = iniciais
-        self.ginasios = ginasios
+
+        self.ginasios?.append(contentsOf: [pewter, cerulean, vermilion, celadon])
         
         
     }
@@ -62,7 +58,7 @@ class PokeMundo: NSObject {
         print("Nome do Ginasio\t    Lider do Ginásio\n")
         print("---------------------------------------\n")
         for ginasio in self.ginasios!{
-            print("%-17s\t%-20s\n", (ginasio as! Gym).name, (ginasio as! Gym).leader.nome)
+            print("%-17s\t%-20s\n", ginasio.name, ginasio.leader.nome)
         }
     }
 }
